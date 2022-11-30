312e1cf5d176c181aa651eae2ffbef74c9ca5f02
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-11-30 09:56:48 -0700

    complete in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [colorObj, setColorObj] = useState({
+    red: '',
+    yellow: '',
+    green: ''
+  })
+  const handleClick = (e) => {
+    if (e.target.id === 'stop') setColorObj({
+      red: 'red',
+      yellow: '',
+      green: ''
+    })
+    else if (e.target.id === 'slow') setColorObj({
+      red: '',
+      yellow: 'yellow',
+      green: ''
+    })
+    else setColorObj({
+      red: '',
+      yellow: '',
+      green: 'green'
+    })
+  }
   return (
     <div className="App">
       <section>
         <div id="control-panel">
-          <h2 id="stop" className="button">
+          <h2 onClick={handleClick} id="stop" className="button">
             Stop
           </h2>
-          <h2 id="slow" className="button">
+          <h2 onClick={handleClick} id="slow" className="button">
             Slow
           </h2>
-          <h2 id="go" className="button">
+          <h2 onClick={handleClick} id="go" className="button">
             Go
           </h2>
         </div>
         <div id="traffic-lights">
-          <div className="bulb"></div>
-          <div className="bulb"></div>
-          <div className="bulb"></div>
+          <div style={{backgroundColor:colorObj.red}} className="bulb"></div>
+          <div style={{backgroundColor:colorObj.yellow}} className="bulb"></div>
+          <div style={{backgroundColor:colorObj.green}} className="bulb"></div>
         </div>
       </section>
     </div>
